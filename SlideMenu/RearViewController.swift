9fc440f0ce137cd4002b1cbbed04f085cffd40e2
Md Sazzad Islam <sazzad@macvm>
2017-01-23 23:33:35 +0600

    init working

---
+++ SlideMenu/RearViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RearViewController.swift
+//  SlideMenu
+//
+//  Created by Md Sazzad Islam on 1/23/17.
+//  Copyright © 2017 lynas. All rights reserved.
+//
+
+import UIKit
+
+class RearViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
